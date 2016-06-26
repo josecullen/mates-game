eba1dcb18ebfc41ab0dc8c905b1dcea5fa73e0af
josecullen <jose.cullen@avantrip.com>
2016-06-26 12:47:42 -0300

    Se agrega step_04 de tutorial meteor angular2

--- client/app.ts
+++ client/app.ts
@@ -4,18 +4,23 @@ import { Component } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { Parties }   from '../collections/parties';
 import { Mongo }     from 'meteor/mongo';
+import { PartiesForm } from './imports/parties-form/parties-form';
 
-//import { bootstrap } from '@angular/platform-browser-dynamic';
 
 @Component({
     selector: 'app',
-    templateUrl: '/client/app.html'
+    templateUrl: '/client/app.html',
+    directives: [PartiesForm]
 })
 class Socially {
     parties: Mongo.Cursor;
 
     constructor () {
         this.parties = Parties.find();
+    }
+
+    removeParty(party) {
+        Parties.remove(party._id);
     }
 
 }
