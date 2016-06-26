151f242214b54c7995736a8dd91453d05c227fd4
josecullen <jose.cullen@avantrip.com>
2016-06-26 11:46:51 -0300

    Base sobre tutorial meteor angular2

---
+++ client/app.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Parties }   from '../collections/parties';
+import { Mongo }     from 'meteor/mongo';
+
+//import { bootstrap } from '@angular/platform-browser-dynamic';
+
+@Component({
+    selector: 'app',
+    templateUrl: '/client/app.html'
+})
+class Socially {
+    parties: Mongo.Cursor;
+
+    constructor () {
+        this.parties = Parties.find();
+    }
+
+}
+
+bootstrap(Socially);
